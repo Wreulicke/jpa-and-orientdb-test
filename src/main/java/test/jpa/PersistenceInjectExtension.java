a325d5d7df6de942b8e40a768f1c60e6d4598d75
Wreulicke <masaya2dies@gmail.com>
2016-08-06 02:56:41 +0900

    modified extension

--- src/main/java/test/jpa/PersistenceInjectExtension.java
+++ src/main/java/test/jpa/PersistenceInjectExtension.java
@@ -9,36 +9,42 @@ import java.util.stream.Collectors;
 
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.InjectionException;
 import javax.enterprise.inject.spi.AnnotatedField;
 import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.enterprise.inject.spi.InjectionTarget;
 import javax.enterprise.inject.spi.ProcessInjectionTarget;
+import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
 public class PersistenceInjectExtension implements Extension {
   Map<Class<?>, List<Field>> pool=new HashMap<>();
   
-  public <Type> void test(@Observes ProcessInjectionTarget<Type> a) {
-    AnnotatedType<Type> type = a.getAnnotatedType();
+  public <Type> void test(@Observes ProcessInjectionTarget<Type> processInjectionTarget) {
+    AnnotatedType<Type> type = processInjectionTarget.getAnnotatedType();
     Class<?> clazz=type.getJavaClass();
     if(pool.containsKey(clazz))return;
-    List<Field> fields=type.getFields().stream().filter(field -> field.isAnnotationPresent(PersistenceContext.class)).map(AnnotatedField::getJavaMember).collect(Collectors.toList());
+    List<Field> fields=type.getFields().stream()
+        .filter(field -> field.isAnnotationPresent(PersistenceContext.class))
+        .map(AnnotatedField::getJavaMember)
+        .filter(field -> field.getType().isAssignableFrom(EntityManager.class))
+        .collect(Collectors.toList());
     if(fields.isEmpty())return;
     pool.put(clazz,fields);
-    final InjectionTarget<Type> injectionTarget = a.getInjectionTarget();
-    a.setInjectionTarget(new TrasferedInjector<>(a.getInjectionTarget(),(instance,ctx) ->{
+    final InjectionTarget<Type> injectionTarget = processInjectionTarget.getInjectionTarget();
+    processInjectionTarget.setInjectionTarget(new TrasferedInjector<>(injectionTarget, (instance,ctx) ->{
       List<Field> list=pool.get(clazz);
-      for (Field field : list) {
+      list.forEach(field->{
         field.setAccessible(true);
         try {
           field.set(instance,Persistence.createEntityManagerFactory("test").createEntityManager());
         } catch (Exception e) {
-          e.printStackTrace();
+          throw new InjectionException(e);
         }
-      }
+      });
     }));
   }
   
